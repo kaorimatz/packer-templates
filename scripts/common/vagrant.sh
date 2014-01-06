6050d305fd9d4c30a64212575a6760f5571335e9
kaorimatz <kaorimatz@gmail.com>
2014-01-07 00:52:00 +0900

    Add a build date to the vagrant box

--- scripts/common/vagrant.sh
+++ scripts/common/vagrant.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+date | sudo tee /etc/vagrant_box_build_time
+
 mkdir -p ~/.ssh
 curl -o ~/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
 chmod 700 ~/.ssh/
