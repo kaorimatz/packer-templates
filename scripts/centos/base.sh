e6f929fe82cee730313645022af7001da5fcbec0
kaorimatz <kaorimatz@gmail.com>
2014-01-10 23:54:51 +0900

    Add a template for CentOS 6.5 (x86_64)

---
+++ scripts/centos/base.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+set -x
+
+sudo yum -y install https://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+sudo sed -i -e 's/^enabled=1/enabled=0/' /etc/yum.repos.d/epel.repo
+
+sudo sed -i -e 's,^ACTIVE_CONSOLES=.*$,ACTIVE_CONSOLES=/dev/tty1,' /etc/sysconfig/init
---
+++ scripts/centos/virtualbox.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -x
+
+sudo yum -y --enablerepo=epel install dkms
+sudo yum -y install perl
+
+# Uncomment this if you want to install Guest Additions with support for X
+#sudo yum -y install xorg-x11-server-Xorg
+
+sudo mount -o loop ~/VBoxGuestAdditions.iso /mnt/
+sudo /mnt/VBoxLinuxAdditions.run || :
+sudo umount /mnt/
+rm -f ~/VBoxGuestAdditions.iso
