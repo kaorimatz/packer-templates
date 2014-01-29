83ca63e74c50809d82e9c8f80d17d63883d6aee2
Satoshi Matsumoto <kaorimatz@gmail.com>
2014-01-29 23:47:16 +0900

    Add a template for Ubuntu 10.04.4 (amd64)

---
+++ scripts/ubuntu/base.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+set -x
+
+sudo sed -i -e 's,^\(ACTIVE_CONSOLES="/dev/tty\).*,\11",' /etc/default/console-setup
+for f in /etc/init/tty[^1]*.conf; do sudo mv $f{,.bak}; done
---
+++ scripts/ubuntu/virtualbox.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -x
+
+sudo aptitude -y install dkms
+sudo aptitude -y install make
+
+# Uncomment this if you want to install Guest Additions with support for X
+#sudo aptitude -y install xserver-xorg
+
+sudo mount -o loop ~/VBoxGuestAdditions.iso /mnt/
+sudo /mnt/VBoxLinuxAdditions.run || :
+sudo umount /mnt/
+rm -f ~/VBoxGuestAdditions.iso
