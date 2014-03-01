9c30dd04028040c1862fe57a1c72a2e77aee398a
Satoshi Matsumoto <kaorimatz@gmail.com>
2014-03-01 16:34:51 +0900

    Add a template for Vyatta Core 6.6R1 (amd64)

---
+++ scripts/vyatta-core/base.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+set -x
+
+WRAPPER=/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper
+
+$WRAPPER begin
+$WRAPPER set system package repository daisy components 'main'
+$WRAPPER set system package repository daisy distribution 'daisy'
+$WRAPPER set system package repository daisy url 'http://packages.vyatta.com/vyatta-dev/daisy/unstable'
+$WRAPPER set system package repository squeeze components 'main contrib non-free'
+$WRAPPER set system package repository squeeze distribution 'squeeze'
+$WRAPPER set system package repository squeeze url 'http://mirrors.kernel.org/debian'
+$WRAPPER commit
+$WRAPPER save
+$WRAPPER end
+
+sudo aptitude -y update
+
+sudo sed -i -e 's,^.*:/sbin/getty\s\+.*\s\+tty[2-6],#\0,' /etc/inittab
---
+++ scripts/vyatta-core/virtualbox.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -x
+
+sudo aptitude -y install build-essential
+sudo aptitude -y install dkms
+sudo aptitude -y install linux-vyatta-kbuild
+
+# Uncomment this if you want to install Guest Additions with support for X
+#sudo aptitude -y install xserver-xorg
+
+sudo ln -s /usr/src/linux-image/debian/build/build-amd64-none-amd64-vyatta "/lib/modules/$(uname -r)/build"
+
+sudo mount -o loop ~/VBoxGuestAdditions.iso /mnt/
+yes | sudo /mnt/VBoxLinuxAdditions.run || :
+sudo umount /mnt/
+rm -f ~/VBoxGuestAdditions.iso
