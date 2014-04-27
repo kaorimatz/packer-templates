b3157edbfb74b5192b8c31df412c985ec6e2b3d5
Satoshi Matsumoto <kaorimatz@gmail.com>
2014-04-27 19:59:44 +0900

    raw.github.com has moved to raw.githubusercontent.com

--- scripts/common/vagrant.sh
+++ scripts/common/vagrant.sh
@@ -6,6 +6,6 @@ set -x
 date | sudo tee /etc/vagrant_box_build_time
 
 mkdir -p ~/.ssh
-curl -o ~/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+curl -sLo ~/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
 chmod 700 ~/.ssh/
 chmod 600 ~/.ssh/authorized_keys
