5a5de7c6038462ff291d70fa685cd0cb2fd52089
Satoshi Matsumoto <kaorimatz@gmail.com>
2014-08-02 22:22:54 +0900

    Fedora: enable DKMS

--- scripts/fedora/virtualbox.sh
+++ scripts/fedora/virtualbox.sh
@@ -11,6 +11,13 @@ sudo yum -y install make
 # Uncomment this if you want to install Guest Additions with support for X
 #sudo yum -y install xorg-x11-server-Xorg
 
+# In Fedora 19 or earlier, dkms package provides SysV init script called
+# dkms_autoinstaller that is enabled by default
+if systemctl list-unit-files | grep -q dkms.service; then
+  sudo systemctl start dkms
+  sudo systemctl enable dkms
+fi
+
 sudo mount -o loop ~/VBoxGuestAdditions.iso /mnt/
 sudo /mnt/VBoxLinuxAdditions.run || :
 sudo umount /mnt/
