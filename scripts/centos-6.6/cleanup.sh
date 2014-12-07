3da37364569d9de08934cb24135512b1c4617e77
Satoshi Matsumoto <kaorimatz@gmail.com>
2014-12-07 14:58:33 +0900

    CentOS: remove old kernels

--- scripts/centos-6.6/cleanup.sh
+++ scripts/centos-6.6/cleanup.sh
@@ -3,6 +3,10 @@
 set -e
 set -x
 
+if rpm -q --whatprovides kernel | grep -Fqv $(uname -r); then
+  rpm -q --whatprovides kernel | grep -Fv $(uname -r) | xargs sudo yum -y remove
+fi
+
 sudo yum --enablerepo=epel clean all
 sudo yum history new
 sudo truncate -c -s 0 /var/log/yum.log
