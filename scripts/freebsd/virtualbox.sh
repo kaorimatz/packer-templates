01e6edecf8b23bc63303d1444474de422609b087
Satoshi Matsumoto <kaorimatz@gmail.com>
2014-12-19 13:20:29 +0900

    FreeBSD: install latest virtualbox-ose-additions

--- scripts/freebsd/virtualbox.sh
+++ scripts/freebsd/virtualbox.sh
@@ -3,7 +3,8 @@
 set -e
 set -x
 
-sudo pkg install -y "virtualbox-ose-additions-$(cat ~/.vbox_version)"
+sudo pkg install -y "virtualbox-ose-additions"
+
 sudo tee -a /etc/rc.conf <<EOF
 vboxguest_enable="YES"
 vboxservice_enable="YES"
