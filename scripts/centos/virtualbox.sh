216c6b2af0fa89423243707e4a477b3a03b84b22
Satoshi Matsumoto <kaorimatz@gmail.com>
2015-01-04 14:01:55 +0900

    CentOS 7: enable DKMS

--- scripts/centos/virtualbox.sh
+++ scripts/centos/virtualbox.sh
@@ -12,6 +12,13 @@ sudo yum -y install perl
 # Uncomment this if you want to install Guest Additions with support for X
 #sudo yum -y install xorg-x11-server-Xorg
 
+# In CentOS 6 or earlier, dkms package provides SysV init script called
+# dkms_autoinstaller that is enabled by default
+if systemctl list-unit-files | grep -q dkms.service; then
+  sudo systemctl start dkms
+  sudo systemctl enable dkms
+fi
+
 sudo mount -o loop,ro ~/VBoxGuestAdditions.iso /mnt/
 sudo /mnt/VBoxLinuxAdditions.run || :
 sudo umount /mnt/
