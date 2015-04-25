11971ba51df7e1de058bf83bd9bc32c54af37e07
Satoshi Matsumoto <kaorimatz@gmail.com>
2015-04-25 12:40:15 +0900

    Add a template for Fedora 22 (x86_64)

---
+++ scripts/fedora-22/cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if rpm -q --whatprovides kernel | grep -Fqv $(uname -r); then
+  rpm -q --whatprovides kernel | grep -Fv $(uname -r) | xargs sudo dnf -y erase
+fi
+
+sudo dnf clean all
