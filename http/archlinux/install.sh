0e1231e446d52547f452545a4e58e9bac93ae739
Satoshi Matsumoto <kaorimatz@gmail.com>
2015-06-20 13:17:25 +0900

    Arch Linux: fix swap being broken due to the lack of `swapoff`

--- http/archlinux/install.sh
+++ http/archlinux/install.sh
@@ -3,17 +3,20 @@
 set -e
 set -x
 
-total_memory=$(free -m | awk '/^Mem:/ { print $2 }')
-swap_size=$((total_memory * 2))
+memory_size=$(free -m | awk '/^Mem:/ { print $2 }')
+swap_size=$((memory_size * 2))
 
 parted --script /dev/sda unit MB
 parted --script /dev/sda mktable msdos
 parted --align=cylinder --script /dev/sda mkpart primary 0 $swap_size
 parted --align=cylinder --script /dev/sda mkpart primary $swap_size 100%
-mkswap /dev/sda1 && swapon /dev/sda1
+mkswap /dev/sda1
 mkfs.ext4 /dev/sda2
-
 mount /dev/sda2 /mnt
 pacstrap /mnt base openssh sudo syslinux
+
+swapon /dev/sda1
 genfstab -p /mnt >> /mnt/etc/fstab
+swapoff /dev/sda1
+
 arch-chroot /mnt /bin/bash
