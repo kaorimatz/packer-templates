4955aa8eeb347661eeb0ab1ea96f3e6a42de3b4f
Satoshi Matsumoto <kaorimatz@gmail.com>
2015-06-20 13:26:31 +0900

    Arch Linux: generate /etc/pacman.d/mirrolist with rankmirrors

--- http/archlinux/install.sh
+++ http/archlinux/install.sh
@@ -13,6 +13,10 @@ parted --align=cylinder --script /dev/sda mkpart primary $swap_size 100%
 mkswap /dev/sda1
 mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
+
+curl -fsS https://www.archlinux.org/mirrorlist/?country=all > /tmp/mirrolist
+grep '^#Server' /tmp/mirrolist | sort -R | head -n 50 | sed 's/^#//' > /tmp/mirrolist.50
+rankmirrors -v /tmp/mirrolist.50 | tee /etc/pacman.d/mirrorlist
 pacstrap /mnt base openssh sudo syslinux
 
 swapon /dev/sda1
