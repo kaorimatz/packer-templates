0d52f4730f4e62964ab54dcf1824baa1c8b1c491
Satoshi Matsumoto <kaorimatz@gmail.com>
2015-07-11 21:43:33 +0900

    Arch Linux: don't create /etc/modules-load.d/virtualbox.conf
    VirtualBox modules will be loaded by `vboxservice` service.

--- scripts/archlinux/virtualbox.sh
+++ scripts/archlinux/virtualbox.sh
@@ -4,11 +4,4 @@ set -e
 set -x
 
 sudo pacman -S --noconfirm virtualbox-guest-utils
-
-sudo tee /etc/modules-load.d/virtualbox.conf <<EOF
-vboxguest
-vboxsf
-vboxvideo
-EOF
-
 sudo systemctl enable vboxservice
