2fb96b203cc0bbf9a3c85a3b30866ad2007df348
Satoshi Matsumoto <kaorimatz@gmail.com>
2015-11-28 17:17:12 +0900

    ArchLinux: fix vboxservice not being started by systemd

--- scripts/archlinux/virtualbox.sh
+++ scripts/archlinux/virtualbox.sh
@@ -5,3 +5,11 @@ set -x
 
 sudo pacman -S --noconfirm virtualbox-guest-utils
 sudo systemctl enable vboxservice
+
+sudo mkdir /etc/systemd/system/vboxservice.service.d/
+sudo tee /etc/systemd/system/vboxservice.service.d/50-reset-condition-virtualization.conf <<EOF
+[Unit]
+# VirtualBox with KVM is detected as "kvm". This should be fixed in systemd 229
+# by https://github.com/systemd/systemd/pull/1995.
+ConditionVirtualization=
+EOF
