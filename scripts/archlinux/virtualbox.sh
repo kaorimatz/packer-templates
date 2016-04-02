3d5980cf2ca4bd67ff052d783120670f3dba37c8
Satoshi Matsumoto <kaorimatz@gmail.com>
2016-04-02 14:33:50 +0900

    Arch Linux: update to 2016.04.01 ISO

--- scripts/archlinux/virtualbox.sh
+++ scripts/archlinux/virtualbox.sh
@@ -3,13 +3,6 @@
 set -e
 set -x
 
+sudo pacman -S --noconfirm linux-headers
 sudo pacman -S --noconfirm virtualbox-guest-utils-nox
 sudo systemctl enable vboxservice
-
-sudo mkdir /etc/systemd/system/vboxservice.service.d/
-sudo tee /etc/systemd/system/vboxservice.service.d/50-reset-condition-virtualization.conf <<EOF
-[Unit]
-# VirtualBox with KVM is detected as "kvm". This should be fixed in systemd 229
-# by https://github.com/systemd/systemd/pull/1995.
-ConditionVirtualization=
-EOF
