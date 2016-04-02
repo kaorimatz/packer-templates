82126d14e1a8eadcec14f333db87b4a7a1258d36
Satoshi Matsumoto <kaorimatz@gmail.com>
2016-04-02 22:53:53 +0900

    VyOS: get Debian packages from Debian Archive
    Debian 6 Squeeze has reached EOL.

--- scripts/vyos/repo.sh
+++ scripts/vyos/repo.sh
@@ -8,7 +8,7 @@ WRAPPER=/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper
 $WRAPPER begin
 $WRAPPER set system package repository squeeze components 'main contrib non-free'
 $WRAPPER set system package repository squeeze distribution 'squeeze'
-$WRAPPER set system package repository squeeze url 'http://mirrors.kernel.org/debian'
+$WRAPPER set system package repository squeeze url 'http://archive.debian.org/debian'
 $WRAPPER commit
 $WRAPPER save
 $WRAPPER end
