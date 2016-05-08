c8753178c96c4e8c0ae6e8f2fc021f689100ffae
Satoshi Matsumoto <kaorimatz@gmail.com>
2016-05-08 19:55:54 +0900

    OpenBSD: zero out all partitions

--- scripts/openbsd/minimize.sh
+++ scripts/openbsd/minimize.sh
@@ -3,8 +3,10 @@
 set -e
 set -x
 
-sudo dd if=/dev/zero of=/EMPTY bs=1M || :
-sudo rm /EMPTY
+for dir in `mount | awk '{ print $3 }'`; do
+  sudo dd if=/dev/zero of="$dir/EMPTY" bs=1M || :
+  sudo rm "$dir/EMPTY"
+done
 
 swap_device=`swapctl -l | awk '!/^Device/ { print $1 }'`
 sudo swapctl -d "$swap_device"
