e2bd8e5003f7e03480080774040f692dbb619671
Satoshi Matsumoto <kaorimatz@gmail.com>
2016-07-17 12:08:12 +0900

    Arch Linux: build vagrant box for VMWare

--- scripts/archlinux/virtualbox.sh
+++ scripts/archlinux/virtualbox.sh
@@ -3,6 +3,10 @@
 set -e
 set -x
 
+if [ "$PACKER_BUILDER_TYPE" != "virtualbox-iso" ]; then
+  exit 0
+fi
+
 sudo pacman -S --noconfirm linux-headers
 sudo pacman -S --noconfirm virtualbox-guest-utils-nox
 sudo systemctl enable vboxservice
---
+++ scripts/archlinux/vmware.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ "$PACKER_BUILDER_TYPE" != "vmware-iso" ]; then
+  exit 0
+fi
+
+sudo pacman -S --noconfirm open-vm-tools
+sudo systemctl enable vmtoolsd
+sudo mkdir /mnt/hgfs
