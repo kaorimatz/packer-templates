20cb9e3f30d3f096df531c0e796c4cf04174ebd8
Satoshi Matsumoto <kaorimatz@gmail.com>
2017-04-18 23:48:09 +0000

    CentOS 5 has reached EOL

--- scripts/centos-5.11/locale.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-sudo find /usr/lib/locale -mindepth 1 -maxdepth 1 -type d -not -name en_US.utf8 -exec rm -r {} +
-sudo /usr/sbin/build-locale-archive
-sudo rm -r /usr/lib/locale/en_US.utf8
--- scripts/centos-7.3/locale.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-localedef --list-archive | grep -a -v en_US.utf8 | xargs sudo localedef --delete-from-archive
-sudo cp /usr/lib/locale/locale-archive{,.tmpl}
-sudo build-locale-archive
