d2ad8878baf37298a76f2235baf67187b7935c80
Satoshi Matsumoto <kaorimatz@gmail.com>
2017-07-02 10:30:00 +0000

    VyOS: fix the package repository URL

--- scripts/vyos/repo.sh
+++ scripts/vyos/repo.sh
@@ -6,10 +6,10 @@ set -x
 WRAPPER=/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper
 
 $WRAPPER begin
-# The package repository for helium has been moved to dev.packages.vyos.net/vyos
+# The package repository for helium has been moved to dev.packages.vyos.net/legacy/repos/vyos
 $WRAPPER set system package repository community components 'main'
 $WRAPPER set system package repository community distribution 'helium'
-$WRAPPER set system package repository community url 'http://dev.packages.vyos.net/vyos'
+$WRAPPER set system package repository community url 'http://dev.packages.vyos.net/legacy/repos/vyos'
 $WRAPPER set system package repository squeeze components 'main contrib non-free'
 $WRAPPER set system package repository squeeze distribution 'squeeze'
 $WRAPPER set system package repository squeeze url 'http://archive.debian.org/debian'
