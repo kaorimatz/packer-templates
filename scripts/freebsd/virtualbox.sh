3b77a9460f59b5b194fa30ad9dd0f54dcd1e9882
Satoshi Matsumoto <kaorimatz@gmail.com>
2017-08-06 02:14:23 +0000

    FreeBSD: install virtualbox-ose-additions-nox11 instead of virtualbox-ose-additions

--- scripts/freebsd/virtualbox.sh
+++ scripts/freebsd/virtualbox.sh
@@ -7,7 +7,7 @@ if [ "$PACKER_BUILDER_TYPE" != "virtualbox-iso" ]; then
   exit 0
 fi
 
-sudo pkg install -y virtualbox-ose-additions
+sudo pkg install -y virtualbox-ose-additions-nox11
 
 sudo tee -a /etc/rc.conf <<EOF
 vboxguest_enable="YES"
