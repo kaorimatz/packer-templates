c8a088f3da880b77df48ffb23dc3a92ba5748b08
Satoshi Matsumoto <kaorimatz@gmail.com>
2017-08-06 08:02:24 +0000

    FreeBSD: disable Sendmail

--- scripts/freebsd/init.sh
+++ scripts/freebsd/init.sh
@@ -3,4 +3,11 @@
 set -e
 set -x
 
+sudo tee -a /etc/rc.conf <<EOF
+sendmail_enable="NO"
+sendmail_submit_enable="NO"
+sendmail_outbound_enable="NO"
+sendmail_msp_queue_enable="NO"
+EOF
+
 sed -e 's/\(ttyv[^0].*getty.*\)on /\1off/' /etc/ttys | sudo tee /etc/ttys > /dev/null
