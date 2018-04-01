df77888040bd77f48eb4dd6b75e61d33128a24ad
Satoshi Matsumoto <kaorimatz@gmail.com>
2018-04-01 20:10:56 +0000

    Drop support for OmniOS

--- scripts/omnios/minimize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-set -x
-
-sudo dd if=/dev/zero of=/EMPTY bs=1M || :
-sudo rm /EMPTY
--- vagrantfile_templates/omnios_vmware.rb
+++
@@ -1,9 +0,0 @@
-Vagrant.configure('2') do |config|
-  config.vm.synced_folder '', '/vagrant', disabled: true
-  config.vm.provider :vmware_fusion do |v|
-    v.functional_hgfs = false
-  end
-  config.vm.provider :vmware_workstation do |v|
-    v.functional_hgfs = false
-  end
-end
