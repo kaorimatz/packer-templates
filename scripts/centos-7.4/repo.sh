4a48de3401f383796b4d61d406780dfce82b3372
Satoshi Matsumoto <kaorimatz@gmail.com>
2018-06-16 13:31:18 +0000

    CentOS 7.4: update epel-release to 7.11

--- scripts/centos-7.4/repo.sh
+++ scripts/centos-7.4/repo.sh
@@ -3,5 +3,5 @@
 set -e
 set -x
 
-sudo yum -y install https://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-10.noarch.rpm
+sudo yum -y install https://download.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
 sudo sed -i -e 's/^enabled=1/enabled=0/' /etc/yum.repos.d/epel.repo
